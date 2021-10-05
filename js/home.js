daf57c34fffc3a8c9449372926c4d9571120d932
nguyennhatanh2k1 <nguyennhatanh2k1@gmail.com>
2021-10-06 01:14:54 +0700

    update 3

--- js/home.js
+++ js/home.js
@@ -1,72 +1,86 @@
 // Transition "pho-title-logo" when scroll up or down
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    if (currentScrollPos === 0) {
-      document.getElementById("pho_title-h").style.top = "165px";
-      document.getElementById("pho_title-h").style.opacity = "1";
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        // $(".main_links-h").addClass("fixed-h");
+        $(".main_links-h").css("opacity", "1");
+        // $(".fixed-h.nav").css("z-index", "0");
+
+
+        if (currentScrollPos === 0) {
+            document.getElementById("pho_title-h").style.top = "165px";
+            document.getElementById("pho_title-h").style.opacity = "1";
+        }
+    } else {
+        document.getElementById("pho_title-h").style.top = "120px";
+        document.getElementById("pho_title-h").style.opacity = "0";
+        // $(".main_links-h").removeClass("fixed-h");    
+        $(".main_links-h").css("opacity", "0");
+        // $(".fixed-h.nav").css("z-index", "-1");
     }
-  } else {
 
-    document.getElementById("pho_title-h").style.top = "120px";
-    document.getElementById("pho_title-h").style.opacity = "0";
-  }
-  prevScrollpos = currentScrollPos;
 
-  console.log(`current scroll pos relative to edge window: ${currentScrollPos}`);
+    prevScrollpos = currentScrollPos;
 
-  var contentSections = $("section.col_content-h").toArray();
+    // console.log(`current scroll pos relative to edge window: ${currentScrollPos}`);
 
-  // console.log(contentSections);
+    var contentSections = $("section.col_content-h").toArray();
 
-  for (let i = 0; i < contentSections.length; i++) {
+    // console.log(contentSections);
 
-    const boundTop = contentSections[i].getBoundingClientRect().top;
-    const offsetTop = contentSections[i].offsetTop;
+    for (let i = 0; i < contentSections.length; i++) {
 
-    // lấy từ cạnh top của element so với cạnh top của window (sẽ thay đổi khi scroll)
-    console.log(`${i} bound top : ${boundTop} `);
+        const boundTop = contentSections[i].getBoundingClientRect().top;
+        const offsetTop = contentSections[i].offsetTop;
 
-    // lấy từ cạnh top của element so với cạnh top của window (giá trị cứng) (ko sẽ thay đổi khi scroll)
-    console.log(`${i} offset top : ${offsetTop}`);
+        // lấy từ cạnh top của element so với cạnh top của window (sẽ thay đổi khi scroll)
+        // console.log(`${i} bound top : ${boundTop} `);
 
-    console.log(`${i} offset top - bound top : ${contentSections[0].offsetTop - boundTop}`);
+        // lấy từ cạnh top của element so với cạnh top của window (giá trị cứng) (ko sẽ thay đổi khi scroll)
+        // console.log(`${i} offset top : ${offsetTop}`);
 
-    if (boundTop <= 630) {
+        // console.log(`${i} offset top - bound top : ${contentSections[0].offsetTop - boundTop}`);
 
-      switch (i) {
-        case 0: {
-          $(".background_image-h img").attr("src", "../images/home/Nutrition-Single-Background2.jpg");
-          break;
+        if (boundTop <= 630) {
+
+            switch (i) {
+                case 0:
+                    {
+                        $(".background_image-h img").attr("src", "../images/home/Nutrition-Single-Background2.jpg");
+                        break;
+                    }
+                case 1:
+                    {
+                        $(".background_image-h img").attr("src", "../images/home/background-2D2E30-1.jpg");
+                        break;
+                    }
+                case 2:
+                    {
+                        $(".background_image-h img").attr("src", "../images/home/Home-Background-2.jpg");
+                        break;
+                    }
+                case 3:
+                    {
+                        $(".background_image-h img").attr("src", "../images/home/Menu-Background.jpg");
+                        break;
+                    }
+            }
+
+            $(".background .background_image-h").css("top", `${-boundTop * 0.05}px`)
         }
-        case 1: {
-          $(".background_image-h img").attr("src", "../images/home/background-2D2E30-1.jpg");
-          break;
-        }
-        case 2: {
-          $(".background_image-h img").attr("src", "../images/home/Home-Background-2.jpg");
-          break;
-        }
-        case 3: {
-          $(".background_image-h img").attr("src", "../images/home/Menu-Background.jpg");
-          break;
-        }
-      }
-    } 
-  }
+    }
 
 }
 
 
 // Show header after loaded
 var header = document.querySelector("header");
-window.onunload = function () {
-  header.style.opacity = "0";
+window.onunload = function() {
+    header.style.opacity = "0";
 }
 
 // Click vào button open nav menu 
 $(".main_links-h a:last-child").click(function() {
-  $("#modal_nav_menu-h .nav_right-h").addClass("open_nav_menu-h");
+    $("#modal_nav_menu-h .nav_right-h").addClass("open_nav_menu-h");
 });
-
